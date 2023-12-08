f2a0f366332ffb4c815589611f1f662897312a77
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2023-12-08 18:26:40 +0100

    removed url parser

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI, {});
   } catch (err) {
     console.error(err);
   }

afbbd2df3cc0dbafeb0f7e16e245071b71500c03
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2023-12-08 18:23:15 +0100

    uri fix

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      //   useUnifiedTopology: true,
-      //   useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
     });
   } catch (err) {
     console.error(err);

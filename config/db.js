58731cf2d6d6f2a32e50b1cc90ce98d222b7090d
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2023-12-08 18:15:27 +0100

    uri change

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       //   useUnifiedTopology: true,
       //   useNewUrlParser: true,
     });
